a85592d33f3300be11d0764edea2523bad126479
Jessica Marais <jessica_marais@hotmail.com>
2019-09-09 14:55:45 +0100

    Add method to load data from students.csv

--- directory.rb
+++ directory.rb
@@ -1,3 +1,6 @@
+# Declare an empty array accessible to all methods
+@students = []
+
 # Save the list of students to a CSV file.
 def save_students
   # Open the file for writing
@@ -11,14 +14,14 @@ def save_students
   file.close
 end
 
-# Declare an empty array accessible to all methods
-@students = []
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
+# Load the data from the file
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
+  file.close
 end
 
 def input_students
@@ -36,19 +39,29 @@ def input_students
   end
 end
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
 # Extract code that prints the menu into its own method
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
     puts "9. Exit" # 9 b/c we'll add more items
 end
+
 # Extract code that prints the students into a method
 def show_students
   print_header
   print_student_list
   print_footer
 end
+
 # Extract case statement into a new method.
 def process(selection)
   case selection
@@ -57,7 +70,9 @@ def process(selection)
   when "2"
     show_students
   when "3"
-    save_students 
+    save_students
+  when "4"
+    load_students
   when "9"
     exit # This causes the program to terminate
   else
@@ -71,7 +86,6 @@ def print_header
 end
 
 def print_student_list
-# Removed .with_index method as not required on this part of the exercise.
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
