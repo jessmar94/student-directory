3c67ae6815ddb2f5ffbc53f79c9dcf218b59df79
Jessica Marais <jessica_marais@hotmail.com>
2019-09-09 12:09:57 +0100

    Refactor w/ methods

--- directory.rb
+++ directory.rb
@@ -1,45 +1,52 @@
+# Declare an empty array accessible to all methods
+@students = []
+
 def interactive_menu
-  students = []
   loop do
-  # 1. Print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 b/c we'll add more items
-  # 2. Read the input and save it into a variable
-    user_selection = gets.chomp
-  # 3. Do what the user's asked
-    case user_selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again please"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # Create an empty array
-  students = []
   # Get the first name
   name = gets.chomp
   # While the name is not empty, repeat this code
   while !name.empty? do
     # Add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # Get another name from the user
     name = gets.chomp
   end
-  # Return the array of students
-  students
+end
+
+# Extract code that prints the menu into its own method
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 b/c we'll add more items
+end
+# Extract code that prints the students into a method
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+# Extract case statement into a new method.
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # This causes the program to terminate
+  else
+    puts "I don't know what you meant, please try again"
+  end
 end
 
 def print_header
@@ -47,14 +54,15 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each.with_index(1) do |student, index|
-    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+def print_student_list
+# Removed .with_index method as not required on this part of the exercise.
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
