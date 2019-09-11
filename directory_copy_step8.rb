f4dae13d263aaeaa58270b2cd79b5a93df448edb
Jessica Marais <jessica_marais@hotmail.com>
2019-09-11 17:38:56 +0100

    Additional exercises in step 8. Add # for each student.

---
+++ directory_copy_step8.rb
@@ -0,0 +1,42 @@
+# Add this file so changes can be seen 
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # Create an empty array
+  students = []
+  # Get the first name
+  name = gets.chomp
+  # While the name is not empty, repeat this code
+  while !name.empty? do
+    # Add the student hash to the array
+      students << {name: name, cohort: :november}
+      puts "Now we have #{students.count} students"
+    # Get another name from the user
+    name = gets.chomp
+  end
+  # Return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+# Print a number before each name.
+    students.each.with_index(1) do |student, index|
+        puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+# Nothing happens untill we call the methods
+print_header
+print(students)
+print_footer(students)
