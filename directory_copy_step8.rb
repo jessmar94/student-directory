ba24b2ef37c05964032216015b6d038347e4953d
Jessica Marais <jessica_marais@hotmail.com>
2019-09-11 18:34:09 +0100

    Additional exercises in step8. Only print students whose name begins with 'A'.

--- directory_copy_step8.rb
+++ directory_copy_step8.rb
@@ -1,22 +1,22 @@
-# Add this file so changes can be seen 
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # Create an empty array
   students = []
   # Get the first name
-  name = gets.chomp
+  # .capitalize so .delete_if condition below works, regardless of user input
+  name = gets.chomp.capitalize
   # While the name is not empty, repeat this code
   while !name.empty? do
     # Add the student hash to the array
-      students << {name: name, cohort: :november}
-      puts "Now we have #{students.count} students"
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
     # Get another name from the user
-    name = gets.chomp
+    name = gets.chomp.capitalize
   end
   # Return the array of students
-  students
+  # Program modified to only print students whose name begins with specific letter.
+  students.delete_if { |student| student[:name][0] != "A" }
 end
 
 def print_header
@@ -25,10 +25,10 @@ def print_header
 end
 
 def print(students)
-# Print a number before each name.
-    students.each.with_index(1) do |student, index|
-        puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
+# Print a number before each name
+  students.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
 def print_footer(students)
